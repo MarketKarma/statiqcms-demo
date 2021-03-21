d121ad6763e27aa5f4e8f42796c02a42263bbd44
William Johnson <william.c.johnson92@gmail.com>
2021-03-21 00:25:17 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <i>
+          All data is provided by&nbsp;
+          <a
+            href="https://www.themoviedb.org/documentation/api"
+            target="
+        __blank"
+          >
+            TMBD
+          </a>
+        </i>
+      </p>
+      <Search />
     </div>
   );
 }
---
+++ src/Search.tsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import {
+  Box,
+  Grid,
+  Grommet,
+  Heading,
+  Image,
+  List,
+  Text,
+  TextInput,
+} from "grommet";
+import { grommet } from "grommet/themes";
+import React, { ChangeEvent } from "react";
+
+const getMovies = async (query: string, cb: any) => {
+  console.log(query);
+  const results = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?api_key=8f04f55cb668f5fe095af040343f9960&query=${encodeURIComponent(
+      query
+    )}/`
+  );
+  cb(results.data.results);
+};
+
+const Search = () => {
+  const [query, setQuery] = React.useState("");
+  const [movies, setMovies] = React.useState([]);
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    getMovies(event.target.value, setMovies);
+  };
+
+  return (
+    <Grommet full theme={grommet}>
+      <Box background="light-1" direction="row" height="100%">
+        <Grid
+          fill
+          rows={["xsmall", "xsmall", "small"]}
+          columns={["500px"]}
+          justifyContent="center"
+          areas={[
+            { name: "title", start: [0, 0], end: [0, 0] },
+            { name: "query", start: [0, 1], end: [0, 1] },
+            { name: "results", start: [0, 2], end: [0, 2] },
+          ]}
+        >
+          <Box
+            gridArea="title"
+            pad={{ horizontal: "medium", vertical: "small" }}
+          >
+            <Heading margin="none">Movie DB</Heading>
+          </Box>
+          <Box
+            gridArea="query"
+            direction="row"
+            align="center"
+            justify="between"
+            pad={{ horizontal: "medium", vertical: "small" }}
+          >
+            <TextInput
+              placeholder="Search for movies"
+              value={query}
+              onChange={onChange}
+            />
+          </Box>
+          <Box gridArea="results">
+            <List data={movies}>
+              {(data: any) => (
+                <Box direction="row" justify="between">
+                  <Image
+                    style={{ width: 75 }}
+                    src={`https://image.tmdb.org/t/p//w220_and_h330_face/${data.poster_path}`}
+                  />
+                  <Text weight="bold">{data.title}</Text>
+                  <Text>{data.release_date}</Text>
+                </Box>
+              )}
+            </List>
+          </Box>
+        </Grid>
+      </Box>
+    </Grommet>
+  );
+};
+
+export default Search;
